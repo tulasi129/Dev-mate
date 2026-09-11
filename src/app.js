7557089027ead83e36a0f0ea5bdc769c09748bde
tulasi129 <saitejtulasig@gmail.com>
2026-09-11 17:54:51 +0530

    basic user routed creation

--- src/app.js
+++ src/app.js
@@ -2,14 +2,30 @@ const express = require('express')
 
 const app = express()
 
-app.use("/", (req,res)=>{
-    res.send("Hello Worlds! This is my first express server...")
+
+
+app.get("/user", (req,res)=>{
+    res.send("User info.....")
 })
 
-app.use("/test", (req,res)=>{
-    res.send("Welcome to the devMate server")
+app.post("/user", (req,res)=>{
+    res.send("Usrer created successfully")
 })
+
+app.delete("/user", (req,res)=>{
+    res.send("User deleted successfully")
+})
+
+app.patch("/user", (req,res)=>{
+    res.send("User updated successfully")
+})
+
+// app.use("/", (req,res)=>{
+//     res.send("Hello Worlds! This is my first express server...")
+// })
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
+
+
