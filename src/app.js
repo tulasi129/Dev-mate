cb9a7a5a3df3c7d389c91d96ae9eedc2824d12e1
tulasi129 <saitejtulasig@gmail.com>
2026-09-15 17:56:06 +0530

    multiple route hanlders

--- src/app.js
+++ src/app.js
@@ -1,31 +1,46 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-
-
-
-app.get("/user/:userId", (req,res,next)=>{
-   res.send(`User id is ${req.params.userId}, username is ${req.query.username}, password is ${req.query.password}`)
-})
-
-app.post("/user", (req,res)=>{
-    res.send("Usrer created successfully")
-})
-
-app.delete("/user", (req,res)=>{
-    res.send("User deleted successfully")``
-})
-
-app.patch("/user", (req,res)=>{
-    res.send("User updated successfully")
-})
-
-// app.use("/", (req,res)=>{
-//     res.send("Hello Worlds! This is my first express server...")
-// })
-
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
-})
+const app = express();
 
 
+
+//Hanlding multiple middleware functions ( route handlers ) for a single route
+
+// app.use('/router',rh1,[rh2,rh3],rh4,rh5) // Grouping route handlers for a single route
+app.use(
+  "/test",
+  (req, res, next) => {
+    next();
+  },
+  (req, res, next) => {
+    next();
+  },
+  (req, res, next) => {
+    next();
+  },
+  (req, res, next) => {
+    res.send("response 4");
+  },
+);
+
+app.get("/user/:userId", (req, res, next) => {
+  res.send(
+    `User id is ${req.params.userId}, username is ${req.query.username}, password is ${req.query.password}`,
+  );
+});
+
+app.post("/user", (req, res) => {
+  res.send("Usrer created successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted successfully")``;
+});
+
+app.patch("/user", (req, res) => {
+  res.send("User updated successfully");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
