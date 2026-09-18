1be67dfed24afd9b922d1f136d46fdc2cca8d154
tulasi129 <saitejtulasig@gmail.com>
2026-09-18 23:39:40 +0530

    addec try cathc block for singnup route

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ const User = require("./models/user");
 // Midldlware helps to parse incoming request body to json object to access the json data in it
 app.use(express.json());
 
-app.use("/test",(req,res)=>{
+app.use("/test", (req, res) => {
   res.send("test route is working");
-})
+});
 
 app.post("/signup", async (req, res) => {
   console.log("Request body:", req.body); // Log the request body to see what data is being sent
@@ -26,14 +26,19 @@ app.post("/signup", async (req, res) => {
   //Create a new user instance using the User model and passing userobject as an argument to the constructor. This will create a new user document in the database with the provided data.
   const user = new User(userObject);
 
-  await user.save()
-  .then(()=>{
-    res.status(200).json({ message: "User created successfully" });
-  })
-  .catch((err) => {
-    res.status(400).json({ message: "Error creating user", error: err.message });
-  });
-
+  try {
+    await user.save()
+      .then(() => {
+        res.status(200).json({ message: "User created successfully" });
+      })
+      .catch((err) => {
+        res
+          .status(400)
+          .json({ message: "Error creating user", error: err.message });
+      });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error", error: err.message });
+  }
 });
 
 dbConnect()
