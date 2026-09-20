4335980a6bc4047c6d8a81ff73272332606f191d
tulasi129 <saitejtulasig@gmail.com>
2026-09-20 16:51:46 +0530

    added  validator package for validation check

--- src/models/user.js
+++ src/models/user.js
@@ -1,59 +1,74 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
-  firstName: {
-    type: String,
-    required : true,
-    minLength: 4,
-    lowerCase :true,
-    trim: true,
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      lowerCase: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address" + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 60,
+    },
+    gender: {
+      type: String,
+      validate: function (value) {
+        if (!["male", "female", "others"].includes(value.toLowerCase())) {
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/045/711/185/non_2x/male-profile-picture-placeholder-for-social-media-forum-dating-site-chat-operator-design-social-profile-template-default-avatar-icon-flat-style-free-vector.jpg",
+    },
+    about: {
+      type: String,
+      default: "This is a default value of user",
+    },
+    skills: {
+      type: [String],
+      required: true,
+      default: undefined,
+      enum: [
+        "html",
+        "javascript",
+        "react",
+        "vue",
+        "animator",
+        "desinger",
+        "electrician",
+        "gamer",
+        "Coder",
+        "bim",
+      ],
+    },
   },
-  lastName: {
-    type: String,
-    trim: true
-
+  {
+    timestamps: true,
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  age: {
-    type: Number,
-    min:18,
-    max:60
-  },
-  gender: {
-    type: String,
-    validate: function(value){
-      if(!['male','female', "others"].includes(value.toLowerCase())){
-        throw new Error("Invalid gender")
-      }
-    }
-  },
-  photoUrl:{
-    type:String,
-    default:'https://static.vecteezy.com/system/resources/previews/045/711/185/non_2x/male-profile-picture-placeholder-for-social-media-forum-dating-site-chat-operator-design-social-profile-template-default-avatar-icon-flat-style-free-vector.jpg'
-  },
-  about:{
-    type:String,
-    default : 'This is a default value of user'
-  },
-  skills:{
-    type: [String],
-    required : true,
-    default: undefined,
-    enum : ["html", "javascript", "react","vue","animator", "desinger", "electrician", "gamer", "Coder","bim" ],
-  }
-  
-},
-{
-    timestamps:true
-  }
 );
 
-
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
